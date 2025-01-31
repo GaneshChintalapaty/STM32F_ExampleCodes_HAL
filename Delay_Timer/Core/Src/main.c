cd6d88db285b76570a472c6267f5663e89cd6a1d
GaneshChintalapaty <ganesh@origenenergysolutions.com>
2025-01-31 12:57:48 -0600

    Timer using Polling mode example added with project report 01312025_1257

--- Delay_Timer/Core/Src/main.c
+++ Delay_Timer/Core/Src/main.c
@@ -102,8 +102,8 @@ int main(void)
   {
     /* USER CODE END WHILE */
 
-	  timerDelay();	//1 second delay
-	  toggleLED();	//Toggle LED
+	  timerDelay();	//1 Second delay
+	  toggleLED();	//Toggle GPIO
 
     /* USER CODE BEGIN 3 */
   }
@@ -181,7 +181,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 1439;
+  htim1.Init.Prescaler = 2879;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 62499;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
